f54d2470b22995a78c46ead5df7ac8baf9f1283e
Cl3tus <osternaud.clement@pm.me>
2025-11-18 18:03:56 +0100

    Refactor SketchyBar configuration and plugins
    - Removed deprecated Spotify and Volume scripts.
    - Added new aerospace workspace management scripts for dynamic workspace updates.
    - Introduced icon mapping function for better application icon representation.
    - Updated battery and clock scripts for improved functionality and formatting.
    - Enhanced overall SketchyBar appearance with new default settings and workspace icons.
    - Cleaned up unused scripts and configurations for better maintainability.

--- sketchybar/.config/sketchybar/items/battery.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item battery right \
-    --set battery \
-    update_freq=20 \
-    script="$PLUGIN_DIR/battery.sh"
--- sketchybar/.config/sketchybar/items/clock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item clock right \
-    --set clock \
-    icon=󰃰 \
-    icon.color=0xffed8796 \
-    update_freq=10 \
-    script="$PLUGIN_DIR/clock.sh"
