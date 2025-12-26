9667b95c9d102a3028db4f46efe2341e8e5af7fd
Cl3tus <osternaud.clement@pm.me>
2025-12-26 22:51:29 +0100

    refactor: update configuration for new monitor setup
    This commit updates the configuration files to reflect changes in hardware and software settings following a recent upgrade. Specifically:
    
    - Updated display output references from `DP-1` to `DP-2`.
    - Adjusted window manager (Hyprland) workspace bindings.
    - Modified Waybar configuration to use new monitor for center modules.
    - Changed wallpaper theme to "basement" with updated image and JSON metadata.
    
    These changes ensure that the system is properly configured for optimal performance on the current hardware setup.

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,13 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = "master",
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "lua", "javascript", "typescript", "tsx", "bash" },
-      sync_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.config").setup({
+			ensure_installed = { "lua", "javascript", "typescript", "tsx", "bash" },
+			sync_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
